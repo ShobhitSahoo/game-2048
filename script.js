e9367301941af5cc8c91f8ab26848d8973e6e8eb
ShobhitSahoo <shobhitsahoo46000@gmail.com>
2022-08-08 01:27:44 +0530

    Completed basic game

--- script.js
+++ script.js
@@ -21,15 +21,31 @@ async function handleInput(e) {
     // console.log(e.key);
     switch (e.key) {
         case 'ArrowUp':
+            if (!canMoveUp) {
+                setupInput();
+                return;
+            }
             await moveUp();
             break;
         case 'ArrowDown':
+            if (!canMoveDown) {
+                setupInput();
+                return;
+            }
             await moveDown();
             break;
         case 'ArrowLeft':
+            if (!canMoveLeft) {
+                setupInput();
+                return;
+            }
             await moveLeft();
             break;
         case 'ArrowRight':
+            if (!canMoveRight) {
+                setupInput();
+                return;
+            }
             await moveRight();
             break;
         default:
@@ -37,6 +53,17 @@ async function handleInput(e) {
             return;
     }
     grid.cells.forEach(cell => cell.mergeTiles());
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
+
+    // Setting the loosing condition
+    if (!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()) {
+        newTile.waitForTransition(true).then(() => {
+            alert('You lost!');
+        })
+        return;
+    }
+
     setupInput();
 }
 
@@ -84,3 +111,29 @@ function slideTiles(cells) {
         }))
 }
 
+function canMoveUp() {
+    return canMove(grid.cellsByColumn);
+}
+
+function canMoveDown() {
+    return canMove(grid.cellsByColumn.map(column => [...column].reverse()));
+}
+
+function canMoveLeft() {
+    return canMove(grid.cellsByRow);
+}
+
+function canMoveRight() {
+    return canMove(grid.cellsByRow.map(row => [...row].reverse()));
+}
+
+function canMove() {
+    return cells.some(group => {
+        return group.some((cell, index) => {
+            if (index === 0) return false;
+            if (cell.tile == null) return false;
+            const moveToCell = group[index - 1];
+            return moveToCell.canAccept(cell.tile);
+        });
+    });
+}
