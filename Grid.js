7dfe569872948c5df57fdf4dca221bb5725bf1f0
ShobhitSahoo <shobhitsahoo46000@gmail.com>
2022-08-08 01:06:15 +0530

    Added promises to adjust Transition

--- Grid.js
+++ Grid.js
@@ -14,6 +14,10 @@ export default class Grid {
         });
     }
 
+    get cells() {
+        return this.#cells;
+    }
+
     get cellsByRow() {
         return this.#cells.reduce((cellGrid, cell) => {
             cellGrid[cell.y] = cellGrid[cell.y] || [];
@@ -85,6 +89,13 @@ class Cell {
 
     canAccept(tile) {
         return (this.tile == null || (this.mergeTile == null && this.tile.value == tile.value));
+    }
+
+    mergeTiles() {
+        if (this.tile == null || this.mergeTile == null) return;
+        this.tile.value = this.tile.value + this.mergeTile.value;
+        this.mergeTile.remove();
+        this.mergeTile = null;
     }
 }
 
--- script.js
+++ script.js
@@ -17,26 +17,26 @@ function setupInput() {
     window.addEventListener('keydown', handleInput, { once: true });
 }
 
-function handleInput(e) {
+async function handleInput(e) {
     // console.log(e.key);
     switch (e.key) {
         case 'ArrowUp':
-            moveUp();
+            await moveUp();
             break;
         case 'ArrowDown':
-            moveDown();
+            await moveDown();
             break;
         case 'ArrowLeft':
-            moveLeft();
+            await moveLeft();
             break;
         case 'ArrowRight':
-            moveRight();
+            await moveRight();
             break;
         default:
             setupInput();
             return;
     }
-    // 
+    grid.cells.forEach(cell => cell.mergeTiles());
     setupInput();
 }
 
@@ -57,26 +57,30 @@ function moveRight() {
 }
 
 function slideTiles(cells) {
-    cells.forEach(group => {
-        for (let i = 1; i < group.length; i++) {
-            const cell = group[i];
-            if (cell.tile == null) continue;
-            let lastValidCell = null;
-            for (let j = i - 1; j >= 0; j--) {
-                const moveToCell = group[j];
-                if (!moveToCell.canAccept(cell.tile)) break;
-                lastValidCell = moveToCell;
-            }
-
-            if (lastValidCell != null) {
-                if (lastValidCell.tile != null) {
-                    lastValidCell.mergeTile = cell.tile;
-                } else {
-                    lastValidCell.tile = cell.tile;
+    return Promise.all(
+        cells.flatMap(group => {
+            const promises = [];
+            for (let i = 1; i < group.length; i++) {
+                const cell = group[i];
+                if (cell.tile == null) continue;
+                let lastValidCell = null;
+                for (let j = i - 1; j >= 0; j--) {
+                    const moveToCell = group[j];
+                    if (!moveToCell.canAccept(cell.tile)) break;
+                    lastValidCell = moveToCell;
                 }
-                cell.tile = null;
+
+                if (lastValidCell != null) {
+                    promises.push(cell.tile.waitForTransition());
+                    if (lastValidCell.tile != null) {
+                        lastValidCell.mergeTile = cell.tile;
+                    } else {
+                        lastValidCell.tile = cell.tile;
+                    }
+                    cell.tile = null;
+                }
             }
-        }
-    });
+            return promises;
+        }))
 }
 
